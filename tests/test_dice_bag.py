dcd75e823ca8d8f2ab80049fd7c974c4da6492fe
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2022-04-08 18:03:41 +0200

    init

--- tests/test_dice_bag.py
+++ tests/test_dice_bag.py
@@ -1,9 +1,10 @@
 import pytest
 
-from src.dice_bag import RollableDice
+from src.dice_bag import RollableDice, DiceBag
 
 
 def test_default_dice_sides():
     dice = RollableDice()
     assert dice.sides == 6
     print("test run successfully")
+    test_bag = DiceBag()
